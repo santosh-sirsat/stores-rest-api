49bcbb20e1944af244315f5b9996413e741991cf
Sirsat Santosh <santosh.sirsat@tieto.com>
2018-10-28 23:54:03 +0530

    Added PostgresSQL environment variable  and psycopg2.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os;
 from flask import Flask;
 from flask_restful import Api;
 from flask_jwt import JWT
@@ -12,7 +13,7 @@ app = Flask(__name__);
 
 
 #start :: SQLALCHEMY param setting
-app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///data.db';
+app.config['SQLALCHEMY_DATABASE_URI']= os.environ.get('DATABASE_URL', 'sqlite:///data.db');
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False;
 #end:: SQLALCHEMY param setting
 
